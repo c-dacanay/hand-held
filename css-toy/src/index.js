52fce3fdfe8206d2598489510b2d1553017e20db
Christina Dacanay <tina.dacanay@gmail.com>
2020-09-18 17:10:06 -0400

    A lot of goofing around

--- css-toy/src/index.js
+++ css-toy/src/index.js
@@ -1,15 +1,31 @@
 //CSS slider help: https://codepen.io/lucas-miranda-the-encoder/pen/OJLBWYd
+//Gradients: http://127.0.0.1:5500/css-toy/index.html
+
+
+//Questions: when I set a background-image gradient, opacity of the 1st div could make it impossible to see divs below.
 
 function init() {
-  let innerImg = document.getElementById("inner-img");
-  let inputBook = document.getElementById("img-input");
+  let l1Img = document.getElementById("layer1");
+  let l1Input = document.getElementById("l1-input");
+
+  let l2Img = document.getElementById("layer2");
+  let l2Input = document.getElementById("l2-input");
+
+  let l3Img = document.getElementById("layer3");
+  let l3Input = document.getElementById("l3-input");
 
   function update() {
-    innerImg.style.opacity = inputBook.value;
-    // console.log("input changed");
+    l1Img.style.opacity = l1Input.value;
+    l2Img.style.opacity = l2Input.value;
+    l3Img.style.opacity = l3Input.value;
+    // console.log("l1Img", l1Img.style.opacity);
+    // console.log("l2Img", l2Img.style.opacity);
+    // console.log("l3Img", l3Img.style.opacity);
   }
 
-  inputBook.addEventListener("input", update);
+  l1Input.addEventListener("input", update);
+  l2Input.addEventListener("input", update);
+  l3Input.addEventListener("input", update);
 
 }
 
