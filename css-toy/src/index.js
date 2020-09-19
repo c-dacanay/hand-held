8bcdda3c452990f7dec035d9d1c46b4c846695fa
Christina Dacanay <tina.dacanay@gmail.com>
2020-09-19 13:46:58 -0400

    Drilling into children SVGs

--- css-toy/src/index.js
+++ css-toy/src/index.js
@@ -1,9 +1,14 @@
 //CSS slider help: https://codepen.io/lucas-miranda-the-encoder/pen/OJLBWYd
 //Gradients: http://127.0.0.1:5500/css-toy/index.html
+//Toggle Animation: https://tympanus.net/codrops/css_reference/animation-play-state/
+//SVGs: http://www.petercollingridge.co.uk/tutorials/svg/interactive/javascript/
 
 
 //Questions: when I set a background-image gradient, opacity of the 1st div could make it impossible to see divs below.
 //How can you pause a CSS animation without losing a position?
+
+let colors = ["#64a999", "#ecebeb", "#eeda7b", "#242424", "#8c9898", "#dd177c", "#60a6bd"]
+
 
 function init() {
   let l1Img = document.getElementById("layer1");
@@ -28,13 +33,23 @@ function init() {
   }
 
   function click() {
-    console.log("hi")
+    //oh man I know there's a way to do this.
+    // for (let i = 0, i < 3, i++) {
+    //   l[i]Img.classList.toggle('paused');
+    // }
+
+    l1kids = l1Img.children;
+
     l1Img.classList.toggle('paused');
     l2Img.classList.toggle('paused');
     l3Img.classList.toggle('paused');
-    console.log(l3Img);
+
+    //set svg color
+    l1kids[0].style.fill = "#64a999";
+    console.log(l1kids[0])
   }
   document.body.addEventListener("mousedown", click);
+
   l1Input.addEventListener("input", update);
   l2Input.addEventListener("input", update);
   l3Input.addEventListener("input", update);
