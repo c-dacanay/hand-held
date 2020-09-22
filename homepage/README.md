8f908bfcae94c90c4272f3bc72ec1161d5314231
Christina Dacanay <tina.dacanay@gmail.com>
2020-09-22 13:59:18 -0400

    Assignment #2

---
+++ homepage/README.md
@@ -0,0 +1,14 @@
+## Mobile Homepage Redesign
+
+
+### Credit
+
+#### Content
+
+
+#### Code
+
+
+## Author
+
+- [Christina Dacanay](http://cdacanay.com/) -- for Hand Held at [NYU ITP](https://itp.nyu.edu)
---
+++ homepage/src/index.js
@@ -0,0 +1,24 @@
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+
+let hour = String(today.getHours());
+let min = String(today.getMinutes());
+
+let time = hour + ':' + min;
+today = mm + '/' + dd;
+
+console.log(today);
+console.log(time);
+
+function init() {
+  let timeBox = document.getElementById('time')
+  let todayBox = document.getElementById('date')
+
+  timeBox.innerHTML = `<p>` + time + `</p`;
+  todayBox.innerHTML = `<p>` + today + `<p>`;
+
+
+}
+
+window.addEventListener("load", init);
